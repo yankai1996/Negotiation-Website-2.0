bd5c5029f9f851f1241764eebe06160d3f7cb4fd
Kai Yan <u3514123@connect.hku.hk>
2018-11-15 16:29:18 +0800

    load default params

--- controllers/router_admin.js
+++ controllers/router_admin.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var getRouter = express.Router();
-var postRouter = express.Router();
-var auth = require('./auth')
-var Instructor = require('../models/instructor');
+const express = require('express');
+const getRouter = express.Router();
+const postRouter = express.Router();
+const auth = require('./auth')
+const Instructor = require('../models/instructor');
+const defaultParams = require('../config').defaultParams;
 
 
 // add games to request
@@ -30,6 +31,7 @@ const checkPaused = async (req, res, next) => {
 const renderAdmin = (req, res, next) => {
     res.render('admin', {
         games: req.games,
+        defaultParams: defaultParams,
         pairs: req.pairs,
         count: req.count,
         paused: req.paused
